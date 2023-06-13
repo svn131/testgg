fd6e62e86e6adfbcdfd4faac198b6929d80fc38f
Vladimir Solovyov <svn131131@gmail.com>
2023-06-13 14:47:57 +0500

    10

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
                         8
                                 9
                                         10
+                                                11
     }
 }
